56c8c22fdaa3f29d71c81db2b463f2424cd70f85
seubpong.mon <pjame.fb@gmail.com>
2022-12-05 09:40:56 +0700

    Added auditlog middleware

--- jenuine-api/Middlewares/AuditLog/AuditLogMiddleware.cs
+++ jenuine-api/Middlewares/AuditLog/AuditLogMiddleware.cs
@@ -15,9 +15,9 @@ namespace Its.Jenuiue.Api.Middlewares.AuditLog
 
         public async Task InvokeAsync(HttpContext httpContext)
         {
-            Log.Information("Begin audit log ...");
+            Log.Information($"Begin audit log ...[{httpContext.Request.Path}]");
             await next(httpContext);
-            Log.Information("End audit log ...");
+            Log.Information($"End audit log [{httpContext.Connection.RemoteIpAddress}]");
         }
     }
 }
