e34eae17d95a3884683c01e4840f98f47882f647
seubpong.mon <pjame.fb@gmail.com>
2022-12-08 11:14:40 +0700

    Added test script

--- scripts/asset-add.bash
+++ scripts/asset-add.bash
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-HOST="james:ThisxIsPassw0rd@localhost:5001"
-#HOST=api.genuine-dev.napbiotec.io
+HOST="james:ThisxIsPassw0rd@localhost:5001" # for local DB only
+HOST="*****:****@api.genuine-dev.napbiotec.io"
 
 id=$(shuf -i 1-100000 -n 1)
 
 #--request POST \
 
+# To add new asset
 curl --header "Content-Type: application/json" \
 -k -v \
---data "{\"ProductId\":\"abcxyz-hygkd-${id}\", \"AssetId\":\"xxxxx-${id}\", \"PinNo\": \"12345670\", \"SerialNo\": \"sasddsssss\", \"IsRegistered\": \"false\", \"AssetName\": \"ss\"}" \
+--data "{\"ProductId\":\"abcxyz-hygkd-${id}\", \"AssetId\":\"xxxxx-${id}\", \"PinNo\": \"pin-00000002\", \"SerialNo\": \"serial-0000002\", \"IsRegistered\": \"false\", \"AssetName\": \"ss\"}" \
 https://${HOST}/api/assets/org/napbiotec/action/AddAsset
