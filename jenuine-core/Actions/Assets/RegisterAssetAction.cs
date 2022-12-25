1521a0f76a3b807c77cb652a5fe88358ff5108ea
seubpong.mon <pjame.fb@gmail.com>
2022-12-25 07:21:11 +0700

    Added statusCode query string

--- jenuine-core/Actions/Assets/RegisterAssetAction.cs
+++ jenuine-core/Actions/Assets/RegisterAssetAction.cs
@@ -11,7 +11,7 @@ namespace Its.Jenuiue.Core.Actions.Assets
 {
     public class RegisterAssetAction : IActionManipulate
     {
-        private readonly string defaultUrl = "https://aldamex.com/product-registration/?status={0}&serial={1}&pin={2}";
+        private readonly string defaultUrl = "https://aldamex.com/product-registration/?status={0}&serial={1}&pin={2}&statusCode={3}";
         private IDatabase dbConn;
         private IMongoDatabase db;
         private string org;
@@ -40,7 +40,7 @@ namespace Its.Jenuiue.Core.Actions.Assets
             return filter;
         }
 
-        private string FormatUrl(string url, string status, string serial, string pin)
+        private string FormatUrl(string url, string status, string serial, string pin, string statusCode)
         {
             var templateUrl = url;
             if (string.IsNullOrEmpty(url))
@@ -51,12 +51,13 @@ namespace Its.Jenuiue.Core.Actions.Assets
             String formattedUrl = String.Format(templateUrl, 
                 EncryptUtil.EncryptString(status, key),
                 EncryptUtil.EncryptString(serial, key), 
-                EncryptUtil.EncryptString(pin, key));
+                EncryptUtil.EncryptString(pin, key),
+                EncryptUtil.EncryptString(statusCode, key));
 
             return formattedUrl;
         }
 
-        private string CreateRedirectUrl(MAsset asset, string status)
+        private string CreateRedirectUrl(MAsset asset, string status, string statusCode)
         {
             string serial = asset.SerialNo;
             string pin = asset.PinNo;
@@ -69,7 +70,7 @@ namespace Its.Jenuiue.Core.Actions.Assets
             if (String.IsNullOrEmpty(asset.ProductId))
             {
                 Log.Error($"Unable to find [ProductId] value for asset serial=[{serial}] pin=[{pin}]");
-                return FormatUrl(defaultUrl, status, serial, pin);
+                return FormatUrl(defaultUrl, status, serial, pin, "ERROR");
             }
 
             var p = new MProduct()
@@ -81,10 +82,10 @@ namespace Its.Jenuiue.Core.Actions.Assets
             if (assetProduct == null)
             {
                 Log.Error($"Unable to find [Production] object for asset serial=[{serial}] pin=[{pin}]");
-                return FormatUrl(defaultUrl, status, serial, pin);
+                return FormatUrl(defaultUrl, status, serial, pin, "ERROR");
             }
 
-            return FormatUrl(assetProduct.RedirectUrl, status, serial, pin);;
+            return FormatUrl(assetProduct.RedirectUrl, status, serial, pin, statusCode);;
         }
 
         public void SetSymetricKey(string keyStr)
@@ -108,7 +109,7 @@ namespace Its.Jenuiue.Core.Actions.Assets
             {
                 status = "Serial and pin not found!!!";
                 (param as MAsset).LastActionStatus = status;
-                (param as MAsset).RedirectUrl = FormatUrl(defaultUrl, status, serial, pin);
+                (param as MAsset).RedirectUrl = FormatUrl(defaultUrl, status, serial, pin, "ERROR");
 
                 Log.Error($"Status=[{status}] serial=[{serial}] pin=[{pin}]");
                 return param;
@@ -118,7 +119,7 @@ namespace Its.Jenuiue.Core.Actions.Assets
             {
                 status = "Found serial and pin more than one instance!!!";
                 (param as MAsset).LastActionStatus = status;
-                (param as MAsset).RedirectUrl = FormatUrl(defaultUrl, status, serial, pin);
+                (param as MAsset).RedirectUrl = FormatUrl(defaultUrl, status, serial, pin, "ERROR");
 
                 Log.Error($"Status=[{status}] serial=[{serial}] pin=[{pin}]");
                 return param;
@@ -132,7 +133,7 @@ namespace Its.Jenuiue.Core.Actions.Assets
                 status = "Serial and pin is already registered!!!";
                 (param as MAsset).LastActionStatus = status;
                 (param as MAsset).RegisteredInfo = (asset as MAsset).RegisteredInfo;
-                (param as MAsset).RedirectUrl = CreateRedirectUrl((asset as MAsset), status);
+                (param as MAsset).RedirectUrl = CreateRedirectUrl((asset as MAsset), status, "ERROR");
 
                 Log.Error($"Status=[{status}] serial=[{serial}] pin=[{pin}]");
                 return param;
@@ -156,7 +157,7 @@ namespace Its.Jenuiue.Core.Actions.Assets
             var updateResult = updateAct.Apply<MAsset>(asset as MAsset);
 
             status = "Serial and pin registered succesfully";
-            updateResult.RedirectUrl = CreateRedirectUrl((asset as MAsset), status);
+            updateResult.RedirectUrl = CreateRedirectUrl((asset as MAsset), status, "SUCCESS");
             Log.Information($"Status=[{status}] serial=[{serial}] pin=[{pin}]");
 
             return (T)(object) updateResult;
