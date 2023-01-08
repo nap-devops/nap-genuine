597b23ee28bcf61432b6f3c57dddecd67879f448
seubpong.mon <pjame.fb@gmail.com>
2023-01-08 16:18:20 +0700

    Update GetAssets query logic

--- jenuine-core/Actions/Assets/GetAssetsAction.cs
+++ jenuine-core/Actions/Assets/GetAssetsAction.cs
@@ -1,5 +1,8 @@
+using System;
 using MongoDB.Driver;
 using Its.Jenuiue.Core.Database;
+using Its.Jenuiue.Core.Models.Organization;
+using System.Collections.Generic;
 
 namespace Its.Jenuiue.Core.Actions.Assets
 {
@@ -14,10 +17,41 @@ namespace Its.Jenuiue.Core.Actions.Assets
         {
             return "assets";
         }
-        
+
         protected override FilterDefinition<T> GetFilter<T>(T model)
         {
-            var filter = FilterDefinition<T>.Empty;
+            if (model == null)
+            {
+                return FilterDefinition<T>.Empty;
+            }
+
+            var m = model as MAsset;
+            List<FilterDefinition<T>> filters = new List<FilterDefinition<T>>();
+
+            if (!String.IsNullOrEmpty(m.PinNo))
+            {
+                var pinNOfilter = Builders<T>.Filter.Where(p => (p as MAsset).PinNo.Contains(m.PinNo));
+                filters.Add(pinNOfilter);
+            }
+                
+            if (!String.IsNullOrEmpty(m.SerialNo))
+            {
+                var serialNofilter = Builders<T>.Filter.Where(p => (p as MAsset).SerialNo.Contains(m.SerialNo));
+                filters.Add(serialNofilter);
+            }
+
+            if (!String.IsNullOrEmpty(m.JobId))
+            {
+                var jobIdfilter = Builders<T>.Filter.Where(p => (p as MAsset).JobId.Contains(m.JobId));
+                filters.Add(jobIdfilter);
+            }
+
+            if (filters.Count <= 0)
+            {
+                return FilterDefinition<T>.Empty;
+            }
+
+            var filter = Builders<T>.Filter.And(filters);
             return filter;
         }
     }
