eb61cbdeee70213e18d25baf67bb7cfb784a1859
pjames <pjames@gmail.com>
2023-05-11 10:40:54 +0700

    Added CLI test command

--- jenuine-cli/run-cli.bash
+++ jenuine-cli/run-cli.bash
@@ -6,6 +6,14 @@
 #./run-cli.bash coa_criteria_group --action=GetCoaCriteriaGroupCount --data=data/coa-criteria-group-query.json
 #./run-cli.bash coa_criteria_group --action=UpdateCoaCriteriaGroupById --id=645c44953752070e1263feb6 --data=data/coa-criteria-group-update.json
 
+#./run-cli.bash coa_criteria --action=GetCoaCriteria --data=data/coa-criteria-query.json
+#./run-cli.bash coa_criteria --action=AddCoaCriteria --data=data/coa-criteria-add.json
+
+#./run-cli.bash coa_criteria --action=DeleteCoaCriteriaById --id=645c41e83752070e1263feb3
+#./run-cli.bash coa_criteria --action=GetCoaCriteriaCount --data=data/coa-criteria-query.json
+#./run-cli.bash coa_criteria --action=UpdateCoaCriteriaById --id=645c44953752070e1263feb6 --data=data/coa-criteria-update.json
+
+
 #./run-cli.bash customer --action=GetCustomers --data=data/customers-query.json
 #./run-cli.bash customer --action=GetProductsCount --data=data/products-query.json
 #./run-cli.bash customer --action=AddProduct --data=data/products-add.json
